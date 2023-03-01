daff82b87d0ec69d32123f437ff65a170430f625
harisishtiaq1 <harisharry232@gmail.com>
2023-03-01 10:57:30 +0500

    Updated

--- src/Components/index.js
+++ src/Components/index.js
@@ -8,7 +8,8 @@ import {
   CardContent,
   Container,
 } from "@mui/material";
-
+import CloseIcon from "@mui/icons-material/Close";
+import haris from "./img/haris.jpeg";
 const App = () => {
   const [myData, setMyData] = useState([]);
   const [isError, setIsError] = useState("");
@@ -33,25 +34,28 @@ const App = () => {
       {isError !== "" && <h2>{isError}</h2>}
       <Container>
         <Grid container spacing={2} sx={{ display: "flex", mt: 2, mb: 2 }}>
-          {myData.slice(0, 100).map((post) => {
+          {/* {myData.slice(0, 100).map((post) => {
             const { url, title } = post;
             return (
-              <Grid item xs={3}>
-                <Card sx={{ maxWidth: 300, height: "100%" }}>
-                  <CardMedia sx={{ height: 300 }} image={url.slice(0, 100)} />
-                  <CardContent>
-                    <Typography
-                      variant="p"
-                      component="p"
-                      color="text.secondary"
-                    >
-                      {title.slice(0, 100)}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
+              
             );
-          })}
+          })} */}
+          <Grid item xs={3}>
+            <Card sx={{ maxWidth: 300, height: "100%" }}>
+              <CardMedia sx={{ height: 300 }} id="image" image={haris} />
+              <CardContent>
+                <Typography variant="p" component="p" color="text.secondary">
+                  abcd1234
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <CardMedia
+            sx={{ width: 500, height: 500 }}
+            id="resizeImg"
+            image={haris}
+          />
+          <CloseIcon sx={{ position: "fixed", right: 30, cursor: "pointer" }} />
         </Grid>
       </Container>
     </>
