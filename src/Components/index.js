23512dea98e613a7fba9321fdc61013a323b6503
harisishtiaq1 <harisharry232@gmail.com>
2023-03-01 12:24:14 +0500

    Updated

--- src/Components/index.js
+++ src/Components/index.js
@@ -7,19 +7,42 @@ import {
   CardMedia,
   CardContent,
   Container,
+  Modal,
+  Pagination,
+  Stack,
 } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-import haris from "./img/haris.jpeg";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  height: 500,
+  bgcolor: "background.paper",
+  borderColor: "white",
+  boxShadow: 24,
+  p: 4,
+  color: "inherit",
+};
+
 const App = () => {
+  const [page, setPage] = useState(1);
   const [myData, setMyData] = useState([]);
   const [isError, setIsError] = useState("");
+  const [imgCard, setImgCard] = useState();
+  const [open, setOpen] = useState(false);
+  const handleOpen = (url) => {
+    setImgCard(url);
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
 
   const getMyPostData = async () => {
     try {
       const res = await axios.get(
-        "https://jsonplaceholder.typicode.com/photos"
+        `https://jsonplaceholder.typicode.com/photos`
       );
-      setMyData(res.data);
+      setMyData(res.data.slice(0, 500));
       console.log("data here", res.data);
     } catch (error) {
       setIsError(error.message);
@@ -29,34 +52,64 @@ const App = () => {
   useEffect(() => {
     getMyPostData();
   }, []);
+
   return (
     <>
       {isError !== "" && <h2>{isError}</h2>}
       <Container>
         <Grid container spacing={2} sx={{ display: "flex", mt: 2, mb: 2 }}>
-          {/* {myData.slice(0, 100).map((post) => {
+          {myData.map((post, index) => {
             const { url, title } = post;
+
             return (
-              
+              <Grid item xs={3}>
+                <Card sx={{ maxWidth: 300, height: "100%" }}>
+                  <CardMedia
+                    sx={{ height: 300 }}
+                    id="image"
+                    onClick={() => handleOpen(url)}
+                    image={url}
+                  />
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                    closeAfterTransition
+                  >
+                    <CardMedia sx={style} image={imgCard} />
+                  </Modal>
+                  <CardContent>
+                    <Typography
+                      variant="p"
+                      component="p"
+                      color="text.secondary"
+                    >
+                      {title}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
             );
-          })} */}
-          <Grid item xs={3}>
-            <Card sx={{ maxWidth: 300, height: "100%" }}>
-              <CardMedia sx={{ height: 300 }} id="image" image={haris} />
-              <CardContent>
-                <Typography variant="p" component="p" color="text.secondary">
-                  abcd1234
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <CardMedia
-            sx={{ width: 500, height: 500 }}
-            id="resizeImg"
-            image={haris}
-          />
-          <CloseIcon sx={{ position: "fixed", right: 30, cursor: "pointer" }} />
+          })}
         </Grid>
+        <Stack
+          spacing={2}
+          sx={{
+            mt: 5,
+            mb:4,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Pagination
+            count={5}
+            color="primary"
+            defaultPage={page}
+            onChange={(event, value) => setPage(value)}
+          />
+        </Stack>
       </Container>
     </>
   );
